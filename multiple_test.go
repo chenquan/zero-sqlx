de4c23a584a0b4aa960b53f4278ad2f694718fdc
chenquan <chenquan.dev@gmail.com>
2023-05-22 22:33:00 +0800

    refactor: refactor the code

--- multiple_test.go
+++ multiple_test.go
@@ -15,44 +15,6 @@ const mockedDatasource = "sqlmock"
 func TestNewMultipleSqlConn(t *testing.T) {
 	leader := "leader"
 	follower1 := "follower1"
-	_, salve1Mock, err := sqlmock.NewWithDSN(follower1, sqlmock.MonitorPingsOption(true))
-	_, leaderMock, err := sqlmock.NewWithDSN(leader, sqlmock.MonitorPingsOption(true))
-	assert.NoError(t, err)
-	salve1Mock.ExpectPing().WillDelayFor(time.Millisecond)
-	leaderMock.ExpectPing().WillDelayFor(time.Millisecond)
-
-	mysql := NewMultipleSqlConn(mockedDatasource, DBConf{
-		Leader:     leader,
-		Followers:  []string{follower1},
-		BackLeader: false,
-		Heartbeat:  time.Minute,
-	})
-
-	salve1Mock.ExpectQuery("select").WillReturnRows(
-		sqlmock.NewRows([]string{"a"}).AddRow("foo"),
-	)
-
-	var result string
-	err = mysql.QueryRow(&result, "select")
-	assert.NoError(t, err)
-	assert.EqualValues(t, "foo", result)
-
-	var list []string
-	err = mysql.QueryRows(&result, "select")
-	assert.NoError(t, err)
-	assert.EqualValues(t, []string{"foo"}, list)
-
-	leaderMock.ExpectExec("any").WillReturnResult(driver.RowsAffected(1))
-	r, err := mysql.Exec("any")
-	assert.NoError(t, err)
-	rowsAffected, err := r.RowsAffected()
-	assert.NoError(t, err)
-	assert.Equal(t, int64(1), rowsAffected)
-
-}
-func TestNewMultipleSqlConn1(t *testing.T) {
-	leader := "leader"
-	follower1 := "follower1"
 	_, follower1Mock, err := sqlmock.NewWithDSN(follower1, sqlmock.MonitorPingsOption(true))
 	_, leaderMock, err := sqlmock.NewWithDSN(leader, sqlmock.MonitorPingsOption(true))
 	assert.NoError(t, err)
